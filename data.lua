cd493f578b9a57164a5711c1f21f9d7ee52ca69e
apo <apophis@tai-wahn.de>
2024-10-06 19:02:48 +0200

    Remove drone explosion

--- data.lua
+++ data.lua
@@ -1,7 +1,6 @@
 require "prototypes.entity.construction-robot"
 require "prototypes.entity.logistic-robot"
 require "prototypes.technology.nuclear-robots"
-require "prototypes.entity.death-explosion"
 
 if settings.startup["bigger-nuclear-cell-stacks"].value == true then
     data.raw.item["uranium-fuel-cell"].stack_size = 200
