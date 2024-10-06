7975d519b3fb998f8860105ff710c9e956aa8f18
apo <apophis@tai-wahn.de>
2024-10-06 19:03:25 +0200

    Remove bigger-nuclear-cell-stacks setting

--- settings.lua
+++ settings.lua
@@ -1,13 +1,6 @@
 data:extend {
     {
         type = "bool-setting",
-        name = "bigger-nuclear-cell-stacks",
-        setting_type = "startup",
-        default_value = false,
-        order = "a",
-    },
-    {
-        type = "bool-setting",
         name = "bigger-cargo",
         setting_type = "startup",
         default_value = false,
