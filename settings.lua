4e992c60fffcbcf9e60a37f44ec9357fd2b560bd
apo <apophis@tai-wahn.de>
2024-10-13 17:14:19 +0200

    Remove payload setting

--- settings.lua
+++
@@ -1,9 +0,0 @@
-data:extend {
-    {
-        type = "bool-setting",
-        name = "bigger-cargo",
-        setting_type = "startup",
-        default_value = false,
-        order = "b",
-    },
-}
